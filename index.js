b6c17685082b7643e2719a189ca1c920d871af03
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2024-01-07 12:25:43 +0530

    reducedsize

--- index.js
+++ index.js
@@ -9,19 +9,17 @@ const { adminlogger } = require("./middelwares/admin.logger")
 const {videoRouter} = require("./routes/videos.routes.js")
 const volenteerRouter = require("./routes/volentire.route")
 const sendEmailRoutes = require("./routes/sendemail.routes")
-const {chatbotRoute} = require("./routes/chatbot.route.js")
 require("dotenv").config()
 app.use(cors())
 app.use(express.json())
 
-// app.use(adminlogger)
 app.use("/admin", adminRouter)
 app.use("/users", userRouter)
 app.use("/volenteer", volenteerRouter)
 app.use("/images", imagesRoute)
 app.use("/videos", videoRouter)
 app.use("/reset_password", sendEmailRoutes)
-app.use("/chatbot",chatbotRoute)
+
 
 app.listen(process.env.port, async () => {
    try {
